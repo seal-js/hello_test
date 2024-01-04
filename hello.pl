ce4a9ae510e9e7bc7c284b391685edb483bee54f
plossys <juergen.stodolka@sealsystems.de>
2024-01-04 12:49:56 +0100

    Add a comment

--- hello.pl
+++ hello.pl
@@ -1,5 +1,6 @@
 use strict;
 
+# This script is stored in a GiHub repository
 print "This is a Perl test file\n";
 
 my @names = ('ich', 'du', 'er');
@@ -7,3 +8,5 @@ my @names = ('ich', 'du', 'er');
 foreach my $ele (@names) {
   print "Name: [$ele]\n";
 }
+
+
